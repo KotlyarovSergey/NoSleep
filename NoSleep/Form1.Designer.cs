5d7da3fd173e7238574517cba844ebe29507e49c
KotlyarovSergey <zews-sv@rambler.ru>
2019-01-16 13:32:16 +0300

    Добавьте файлы проекта.

---
+++ NoSleep/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace NoSleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chbSvernut = new System.Windows.Forms.CheckBox();
+            this.lblVremyaProstoya = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(165, 70);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chbSvernut
+            // 
+            this.chbSvernut.AutoSize = true;
+            this.chbSvernut.BackColor = System.Drawing.Color.Transparent;
+            this.chbSvernut.Checked = true;
+            this.chbSvernut.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbSvernut.Location = new System.Drawing.Point(12, 12);
+            this.chbSvernut.Name = "chbSvernut";
+            this.chbSvernut.Size = new System.Drawing.Size(126, 17);
+            this.chbSvernut.TabIndex = 1;
+            this.chbSvernut.Text = "Сворачивать в трей";
+            this.chbSvernut.UseVisualStyleBackColor = false;
+            // 
+            // lblVremyaProstoya
+            // 
+            this.lblVremyaProstoya.AutoSize = true;
+            this.lblVremyaProstoya.Location = new System.Drawing.Point(9, 120);
+            this.lblVremyaProstoya.Name = "lblVremyaProstoya";
+            this.lblVremyaProstoya.Size = new System.Drawing.Size(35, 13);
+            this.lblVremyaProstoya.TabIndex = 2;
+            this.lblVremyaProstoya.Text = "label1";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "NoSleep остановлен";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NoSleep.Properties.Resources.StartPict;
+            this.pictureBox1.Location = new System.Drawing.Point(183, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 140);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblVremyaProstoya);
+            this.Controls.Add(this.chbSvernut);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NoSleep";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox chbSvernut;
+        private System.Windows.Forms.Label lblVremyaProstoya;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ NoSleep/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace NoSleep
+{
+    public partial class Form1 : Form
+    {
+        public static Boolean NoSleepFlag = false;
+
+        //  -----------     ИМПОРТ БИБЛИОТЕК -------------
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //public static extern int SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE flags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_CONTINUOUS = 0x80000000
+        }
+
+        // Unmanaged function from user32.dll
+        [DllImport("user32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        // Struct we'll need to pass to the function
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+        // -----------------------------------------------
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartStop();
+        }
+
+
+        // Запуск/остановка программы
+        private void StartStop()
+        {
+            // нормальное состояние (нажатие стоп)
+            if (Form1.NoSleepFlag)
+            {
+                NoSleepFlag = false;
+                btnStart.Text = "START";
+                // Отменяем запрет сна
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+                //timer1.Start();
+                pictureBox1.Image = global::NoSleep.Properties.Resources.StartPict;
+                notifyIcon1.Text = "NoSleep остановлен";
+            }
+            else    // Запрет сна!!  (нажатие старт)
+            {
+                NoSleepFlag = true;
+                btnStart.Text = "STOP";
+                lblVremyaProstoya.Text = "Система бодрствует";
+                //timer1.Stop();
+                // Запрещаем переходить в сон:
+                SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+                pictureBox1.Image = global::NoSleep.Properties.Resources.StopPict;
+                notifyIcon1.Text = "NoSleep запущен";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timer1.Stop();
+            // Получаем симтемное время в тактах
+            int systemUptime = Environment.TickCount;
+
+            // Время (в тактах) последнего события
+            int LastInputTicks = 0;
+
+            // разница м/у временем последнего события и ситсемного времени
+            int IdleTicks = 0;
+
+            // Создем структуру LastInputInfo
+            LASTINPUTINFO LastInputInfo = new LASTINPUTINFO();
+            LastInputInfo.cbSize = (uint)Marshal.SizeOf(LastInputInfo);
+            LastInputInfo.dwTime = 0;
+            
+            // Получаем время последнего ввода (события)
+            if (GetLastInputInfo(ref LastInputInfo))
+            {
+                // Get the number of ticks at the point when the last activity was seen
+                LastInputTicks = (int)LastInputInfo.dwTime;
+                
+                // время простоя = разница м\у временем события и текущим временем
+                IdleTicks = systemUptime - LastInputTicks;
+
+                string vrpr = "Время простоя системы: " + Convert.ToString(IdleTicks / 1000) + " сек.";
+                lblVremyaProstoya.Text = vrpr;
+            }
+            //timer1.Start();
+        }
+
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            VosstanovitFormu();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            VosstanovitFormu();
+        }
+
+        // восстанавливаем форму из трея
+        private void VosstanovitFormu()
+        {
+
+            this.Show();
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+            notifyIcon1.Visible = false;
+            timer1.Start();
+
+        }
+
+        // Изменение размерво формы
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // если форма сворачивается
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                   // если сворчиваем в трей
+                if (chbSvernut.Checked)
+                {
+                    notifyIcon1.Visible = true;
+                    this.ShowInTaskbar = false;
+                    this.Hide();
+                    timer1.Stop();
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            StartStop();
+        }
+    }
+}
+
